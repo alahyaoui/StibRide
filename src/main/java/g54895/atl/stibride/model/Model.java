54b33230097879c6214a687649bff9941bee33e2
Juba Ait Atlas <68084375+Juba-ino@users.noreply.github.com>
2021-05-06 23:36:20 +0200

    Ajout du pattern mvp au projet StibRide

---
+++ src/main/java/g54895/atl/stibride/model/Model.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g54895.atl.stibride.model;
+
+import javafx.beans.Observable;
+
+/**
+ *
+ * @author ayoub
+ */
+public interface Model extends Observable {
+
+}
