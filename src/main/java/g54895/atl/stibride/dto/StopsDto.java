fdcb72088138d2eeb919e9a0479e9f5508eac944
54895 <54895@etu.he2b.be>
2021-05-11 10:51:20 +0200

    Correction de la classe StopsDto, ajout d'un attribut stationKey réprésentant la clé id_station.

--- src/main/java/g54895/atl/stibride/dto/StopsDto.java
+++ src/main/java/g54895/atl/stibride/dto/StopsDto.java
@@ -1,5 +1,6 @@
 package g54895.atl.stibride.dto;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,25 +10,56 @@ import java.util.List;
  */
 public class StopsDto extends Dto<Integer> {
 
+    private int stationKey;
     private String stationName;
-    private String lines;
+    private List<Integer> lines;
 
     public StopsDto(Integer key) {
         super(key);
     }
 
-    public StopsDto(Integer key, String stationName, String lines) {
+    public StopsDto(Integer key, String stationName) {
+        super(key);
+        this.stationName = stationName;
+        lines = new ArrayList<>();
+    }
+
+    public StopsDto(Integer key, int stationKey) {
+        super(key);
+        this.stationKey = stationKey;
+        lines = new ArrayList<>();
+    }   
+    
+    public StopsDto(Integer key, int stationKey, List<Integer> lines) {
+        super(key);
+        this.stationKey = stationKey;
+        this.lines = lines;
+    }
+    
+    public StopsDto(Integer key, String stationName, List<Integer> lines) {
         super(key);
         this.stationName = stationName;
         this.lines = lines;
     }
 
-    public String getLines() {
+    public void addLine(int line) {
+        lines.add(line);
+    }
+
+    public List<Integer> getLines() {
         return lines;
+    }
+    
+    public int getLine() {
+        return lines.get(0);
     }
 
     public String getStationName() {
         return stationName;
+    }
+
+    public int getStationKey() {
+        return stationKey;
     }
 
 }
