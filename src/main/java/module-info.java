5713358779b573b89aa86b76952b3ec13f15167e
54895 <54895@etu.he2b.be>
2021-05-11 20:46:39 +0200

    Ajout de SearchableComboBox

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,19 @@
-module g54895.atl.stibride {
+module stibride {
     requires javafx.controls;
     requires javafx.graphics;
     requires java.desktop;
     requires java.base;
     requires javafx.fxml;
     requires java.sql;
-
-    opens g54895.atl.stibride.view to javafx.fxml;
+    requires org.controlsfx.controls;
+    
+//opens g54895.atl.stibride.view to javafx.controls;
+    opens g54895.atl.stibride.view to javafx.fxml, javafx.controls, org.controlsfx.controls;
     exports g54895.atl.stibride.view;
     exports g54895.atl.stibride.main;
+    
+    opens g54895.atl.stibride.dto to javafx.base;
+
 
     opens g54895.atl.stibride.jdbc to java.sql;
     exports g54895.atl.stibride.jdbc;
