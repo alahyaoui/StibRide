90ce1f803eb0d3b66f468f06bdabb58d2a6f8813
54895 <54895@etu.he2b.be>
2021-05-11 10:50:23 +0200

    Correction des classes Dao

--- src/main/java/g54895/atl/stibride/jdbc/StopsDao.java
+++ src/main/java/g54895/atl/stibride/jdbc/StopsDao.java
@@ -29,13 +29,35 @@ public class StopsDao implements Dao<Integer, StopsDto> {
         return StopsDaoHolder.getInstance();
     }
 
-    @Override
-    public List<StopsDto> selectAll() throws RepositoryException {
-        String sql = "SELECT id,firstname,lastname FROM STUDENTS";
+    /*@Override
+    public List<StopsDto> selectAll() throws RepositoryException {//On va avoir des objets en double
+        String sql = "SELECT id_order, stations.name, id_line FROM STOPS "
+                + "JOIN STATIONS ON stops.id_station = stations.id "
+                + "ORDER BY id_line, id_order;";
         List<StopsDto> dtos = new ArrayList<>();
         try ( Statement stmt = connexion.createStatement();  ResultSet rs = stmt.executeQuery(sql)) {
             while (rs.next()) {
-                StopsDto dto = new StopsDto(rs.getInt(1), rs.getString(2), rs.getString(3));
+                StopsDto dto = new StopsDto(rs.getInt(1), rs.getString(2));
+                dto.addLine(rs.getInt(3));
+                
+                dtos.add(dto);
+            }
+        } catch (SQLException e) {
+            throw new RepositoryException(e);
+        }
+        return dtos;
+    }*/
+    
+     @Override
+    public List<StopsDto> selectAll() throws RepositoryException {//On va avoir des objets en double
+        String sql = "SELECT id_order, id_station, id_line FROM STOPS "
+                + "ORDER BY id_line, id_order;";
+        List<StopsDto> dtos = new ArrayList<>();
+        try ( Statement stmt = connexion.createStatement();  ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                StopsDto dto = new StopsDto(rs.getInt(1), rs.getInt(2));
+                dto.addLine(rs.getInt(3));
+                
                 dtos.add(dto);
             }
         } catch (SQLException e) {
@@ -44,24 +66,52 @@ public class StopsDao implements Dao<Integer, StopsDto> {
         return dtos;
     }
 
+
     @Override
     public StopsDto select(Integer key) throws RepositoryException {
         if (key == null) {
             throw new RepositoryException("Aucune clé donnée en paramètre");
         }
-        String sql = "SELECT id,lastname,firstname FROM STUDENTS WHERE  id = ?";
+        String sql = "SELECT id_order, stations.name, id_line FROM STOPS "
+                + "JOIN STATIONS ON id_station = stations.id "
+                + "WHERE id_station = ?;";
+
         StopsDto dto = null;
         try ( PreparedStatement pstmt = connexion.prepareStatement(sql)) {
             pstmt.setInt(1, key);
             ResultSet rs = pstmt.executeQuery();
 
-            int count = 0;
             while (rs.next()) {
-                dto = new StopsDto(rs.getInt(1), rs.getString(2), rs.getString(3));
-                count++;
+                if (dto == null) {
+                    dto = new StopsDto(rs.getInt(1), rs.getString(2));
+                }
+                dto.addLine(rs.getInt(3));
             }
-            if (count > 1) {
-                throw new RepositoryException("Record pas unique " + key);
+        } catch (SQLException e) {
+            throw new RepositoryException(e);
+        }
+        return dto;
+    }
+
+    @Override
+    public StopsDto select(String stationName) throws RepositoryException {
+        if (stationName == null || stationName == "") {
+            throw new RepositoryException("Aucune clé donnée en paramètre");
+        }
+        String sql = "SELECT id_order, stations.name, id_line FROM STOPS "
+                + "JOIN STATIONS ON id_station = stations.id "
+                + "WHERE stations.name = ?;";
+
+        StopsDto dto = null;
+        try ( PreparedStatement pstmt = connexion.prepareStatement(sql)) {
+            pstmt.setString(1, stationName);
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                if (dto == null) {
+                    dto = new StopsDto(rs.getInt(1), rs.getString(2));
+                }
+                dto.addLine(rs.getInt(3));
             }
         } catch (SQLException e) {
             throw new RepositoryException(e);
