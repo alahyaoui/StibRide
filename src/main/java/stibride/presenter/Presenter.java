1ed0b0d0746b483e843e137dfc85dc67e0672732
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-03-30 19:48:47 +0200

    Add documentation to Presenter class and rename some methods

--- src/main/java/stibride/presenter/Presenter.java
+++ src/main/java/stibride/presenter/Presenter.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 import observer.Observable;
 import observer.Observer;
 import stibride.config.ConfigManager;
-import stibride.dto.FavoriteRoutesDto;
+import stibride.dto.FavoriteTripDto;
 import stibride.dto.StationsDto;
 import stibride.exception.RepositoryException;
 import stibride.model.Model;
@@ -18,8 +18,9 @@ import stibride.repository.StationsRepository;
 import stibride.view.View;
 
 /**
- *
- * @author ayoub
+ * The presenter is the link between the view and the model. It is the presenter of the application in the MVP design patter.
+ * 
+ * @author Ayoub Lahyaoui
  */
 public class Presenter implements Observer {
 
@@ -32,13 +33,19 @@ public class Presenter implements Observer {
         initialize();
     }
 
+    /**
+     * Initialize the view and model
+     */
     public void initialize() throws RepositoryException, IOException {
-        //model.initialize();
         model.addObserver(this);
         view.initialize(this);
         initChoiceBox();
     }
 
+    /**
+     * Initialize the choice box with the list of stations
+     * @TODO move the business code in model class
+     */
     private void initChoiceBox() throws RepositoryException, IOException {
         List<String> stations = new ArrayList<>();
 
@@ -63,16 +70,32 @@ public class Presenter implements Observer {
         view.initChoiceBox(stations);
     }
 
+    /**
+     * Calls the `search` function on the `model` object
+     * 
+     * @param origin the id of the origin station
+     * @param destination the id of the destination station
+     */
     public void doResearch(int origin, int destination) {
         model.search(origin, destination);
     }
 
+    /**
+     * Calls the `search` function on the `model` object
+     * 
+     * @param origin the title of the origin station
+     * @param destination the title of the destination station
+     */
     public void doResearch(String origin, String destination) {
         model.search(origin, destination);
     }
 
-    private void initFavoriteRoutesChoiceBox() throws RepositoryException, IOException {
-        List<FavoriteRoutesDto> favoriteRoutes = new ArrayList<>();
+   /**
+    * Loads the favorite trips from the database and displays them in the favorite routes choice box
+    * @TODO move the business code in model class
+    */
+    private void initFavoriteTripsChoiceBox() throws RepositoryException, IOException {
+        List<FavoriteTripDto> favoriteTrips = new ArrayList<>();
 
         try {
             ConfigManager.getInstance().load();
@@ -80,10 +103,10 @@ public class Presenter implements Observer {
             System.out.println("Base de données stockée : " + dbUrl);
 
             FavoriteRoutesRepository repository = new FavoriteRoutesRepository();
-            List<FavoriteRoutesDto> dtos = repository.getAll();
+            List<FavoriteTripDto> dtos = repository.getAll();
 
-            for (FavoriteRoutesDto dto : dtos) {
-                favoriteRoutes.add(dto);
+            for (FavoriteTripDto dto : dtos) {
+                favoriteTrips.add(dto);
             }
 
         } catch (IOException ex) {
@@ -92,30 +115,42 @@ public class Presenter implements Observer {
             System.out.println("Erreur Repository " + ex.getMessage());
         }
 
-        view.initFavoriteRoutesChoiceBox(favoriteRoutes);
+        view.initFavoriteTripsChoiceBox(favoriteTrips);
     }
 
-    public void addFavorite(String origin, String destination) throws RepositoryException, IOException {
-        FavoriteRoutesDto dto = null;
+    /**
+     * Add a favorite trip to the database
+     * 
+     * @param origin the origin of the trip
+     * @param destination the destination of the trip
+     */
+    public void addFavoriteTrip(String origin, String destination) throws RepositoryException, IOException {
+        FavoriteTripDto dto = null;
         try {
             ConfigManager.getInstance().load();
             String dbUrl = ConfigManager.getInstance().getProperties("db.url");
             System.out.println("Base de données stockée : " + dbUrl);
 
             FavoriteRoutesRepository repository = new FavoriteRoutesRepository();
-            //dto = repository.get(origin, destination);
-            repository.add(new FavoriteRoutesDto(0, origin, destination));
+            repository.add(new FavoriteTripDto(0, origin, destination));
 
         } catch (IOException ex) {
             System.out.println("Erreur IO " + ex.getMessage());
         } catch (RepositoryException ex) {
             System.out.println("Erreur Repository " + ex.getMessage());
         }
-        initFavoriteRoutesChoiceBox();
+        initFavoriteTripsChoiceBox();
     }
 
-    public void updateFavorite(int key, String origin, String destination) throws RepositoryException, IOException {
-        FavoriteRoutesDto dto = new FavoriteRoutesDto(key, origin, destination);
+    /**
+     * Update the favorite trip in the database
+     * 
+     * @param key The primary key of the favorite trip to update.
+     * @param origin the origin of the trip
+     * @param destination the destination of the trip
+     */
+    public void updateFavoriteTrip(int key, String origin, String destination) throws RepositoryException, IOException {
+        FavoriteTripDto dto = new FavoriteTripDto(key, origin, destination);
         try {
             ConfigManager.getInstance().load();
             String dbUrl = ConfigManager.getInstance().getProperties("db.url");
@@ -129,11 +164,16 @@ public class Presenter implements Observer {
         } catch (RepositoryException ex) {
             System.out.println("Erreur Repository " + ex.getMessage());
         }
-        initFavoriteRoutesChoiceBox();
+        initFavoriteTripsChoiceBox();
     }
 
-    public void deleteFavorite(FavoriteRoutesDto item) throws RepositoryException, IOException {
-        FavoriteRoutesDto dto = item;
+    /**
+     * Delete a favorite trip from the database
+     * 
+     * @param item The item to be deleted.
+     */
+    public void deleteFavoriteTrip(FavoriteTripDto item) throws RepositoryException, IOException {
+        FavoriteTripDto dto = item;
         try {
             ConfigManager.getInstance().load();
             String dbUrl = ConfigManager.getInstance().getProperties("db.url");
@@ -147,15 +187,20 @@ public class Presenter implements Observer {
         } catch (RepositoryException ex) {
             System.out.println("Erreur Repository " + ex.getMessage());
         }
-        initFavoriteRoutesChoiceBox();
+        initFavoriteTripsChoiceBox();
     }
 
+    /**
+     * Update the table view with the search results
+     * 
+     * @param observable The object that is being observed.
+     * @param arg The argument passed to the update method.
+     */
     @Override
     public void update(Observable observable, Object arg) {
         System.out.println("Update");
         Model savedModel = (Model) observable;
         try {
-            //view.updateTableView(savedModel.getSearchResult2());
             view.updateTableView(savedModel.getSearchResult3());
 
         } catch (IOException ex) {
