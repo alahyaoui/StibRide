47bbd4cb12837e6c0f27fdddd03a78d0dbc839f9
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-03-30 20:21:14 +0200

    Add documentation to model interface

--- src/main/java/stibride/model/Model.java
+++ src/main/java/stibride/model/Model.java
@@ -7,26 +7,77 @@ import stibride.dto.FavoriteTripDto;
 import stibride.dto.StationsDto;
 
 /**
- *
+ * The model of the application in the MVP design patter.
+ *  
  * @author Ayoub Lahyaoui
  */
 public interface Model {
 
+    /**
+     * Given an origin and destination, find the shortest path between them
+     * 
+     * @param idOrigin the id of the origin station
+     * @param idDestination the id of the destination station
+     */
     public void search(int idOrigin, int idDestination);
 
+    /**
+     * Search for the shortest path between origin and destination
+     * 
+     * @param idOrigin the name of the origin station
+     * @param idDestination the name of the destination station
+     */
     public void search(String origin, String destination);
 
+    /**
+     * Get the list of stations that match the search criteria
+     * 
+     * @return A list of stations.
+     */
     public List<StationsDto> getSearchResult();
 
+    /**
+     * Get the metro network
+     * 
+     * @return The network object.
+     */
     public Network getNetwork();
 
+    /**
+     * Get a list of favorite trips
+     * 
+     * @return A list of favorite trips.
+     */
     public List<FavoriteTripDto> getFavoriteTrips();
 
+    /**
+     * Add a favorite trip to the database
+     * 
+     * @param origin The origin of the trip.
+     * @param destination the name of the destination
+     */
     public void addFavoriteTrip(String origin, String destination);
 
+    /**
+     * Update the favorite trip for a user
+     * 
+     * @param key The unique identifier for the trip.
+     * @param origin The origin of the trip.
+     * @param destination the destination of the trip
+     */
     public void updateFavoriteTrip(int key, String origin, String destination);
 
+    /**
+     * Delete a favorite trip
+     * 
+     * @param trip The trip to be deleted.
+     */
     public void deleteFavoriteTrip(FavoriteTripDto trip);
 
+    /**
+     * Add an observer to the list of observers
+     * 
+     * @param observer The observer to be added.
+     */
     public void addObserver(Observer observer);
 }
