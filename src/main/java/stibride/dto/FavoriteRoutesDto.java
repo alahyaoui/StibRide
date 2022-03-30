899363e0a6135eced2bc34f961e873e0384b4b96
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-03-30 18:18:26 +0200

    Add more documentation to FavoriteRoutesDto class

--- src/main/java/stibride/dto/FavoriteRoutesDto.java
+++ src/main/java/stibride/dto/FavoriteRoutesDto.java
@@ -15,17 +15,31 @@ public class FavoriteRoutesDto extends Dto<Integer> {
         this.destination = destination;
     }
 
+    /**
+     * Get the origin of the favorite trip
+     * 
+     * @return The origin of the trip.
+     */
     public String getOrigin() {
         return origin;
     }
 
+    /**
+     * Get the destination of the favorite trip
+     * 
+     * @return The destination of the trip.
+     */
     public String getDestination() {
         return destination;
     }
 
+    /**
+     * Returns a string representation of the trip
+     * 
+     * @return The origin and destination of the trip.
+     */
     @Override
     public String toString() {
         return origin + " -> " + destination;
     }
-
 }
